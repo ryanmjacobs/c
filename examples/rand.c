e1a4218dbf7851ce6ef08b6cf18c7e2d0ee28e65
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-03-01 23:29:58 -0800

    add rand.c

---
+++ examples/rand.c
@@ -0,0 +1,15 @@
+#!/usr/bin/c -std=c99 --
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    puts("Generating 10 random numbers...");
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+        printf("%d\n", rand());
+
+    return 0;
+}
