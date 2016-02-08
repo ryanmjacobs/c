10432b4a021b710b0831bef93b6897995f44cce0
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-02-08 00:12:18 -0800

    tests: remove github_commits.c b/c of library dependencies

--- tests/examples/test.sh
+++ tests/examples/test.sh
@@ -4,8 +4,8 @@ header1 "Test #6: Ensure ./examples are running correctly."
 
 # Sadly, we can't test dinoshade.c or cube.c because they require OpenGL.
 # We can't test noise.sh because it requires aplay.
-files=("dns.c" "github_commits.c" "hello.c" "hello.cpp" "rand.c")
-returns=(1 2 0 0 0 1)
+files=("dns.c" "hello.c" "hello.cpp" "rand.c")
+returns=(1 0 0 0 1)
 
 for i in `seq 0 $((${#files[@]} - 1))`; do
     "../examples/${files[$i]}"; ret=$?
