13a08966d43693bc2351b7c08690d82a8ded2f5c
Ryan Jacobs <ryan.mjacobs@gmail.com>
2016-02-08 00:22:41 -0800

    hstrerror() is deprecated... so we do it ourselves
    definition of h_errno values: http://linux.die.net/man/3/hstrerror

--- examples/dns.c
+++ examples/dns.c
@@ -8,6 +8,27 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+/* http://linux.die.net/man/3/hstrerror */
+void _hstrerror(int err) {
+    switch (err) {
+        case HOST_NOT_FOUND:
+            fputs("error: host is unknown", stderr);
+            break;
+
+        case NO_ADDRESS:
+            fputs("error: name is valid but has no IP address", stderr);
+            break;
+
+        case NO_RECOVERY:
+            fputs("error: nonrecoverable name server error occurred", stderr);
+            break;
+
+        case TRY_AGAIN:
+            fputs("error: temporary error occurred, try again later", stderr);
+            break;
+    }
+}
+
 int main(int argc, char **argv) {
     struct hostent *h;
     struct in_addr**ia;
@@ -23,7 +44,7 @@ int main(int argc, char **argv) {
         ia = (struct in_addr**)h->h_addr_list;
         while (*ia) printf("%s\n", inet_ntoa(**(ia++)));
     } else {
-        printf("%s\n", hstrerror(h_errno));
+        _hstrerror(h_errno);
     }
 
     return 0;
