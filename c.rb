614f45af79d5cf48c936d69465dc36e1519e1fa4
Ryan Jacobs <ryan.mjacobs@gmail.com>
2018-03-17 09:17:00 -0700

    bump version to v0.12

--- c.rb
+++ c.rb
@@ -1,13 +1,14 @@
 class C < Formula
   homepage "https://github.com/ryanmjacobs/c"
-  url "https://github.com/ryanmjacobs/c/archive/v0.11.tar.gz"
-  sha256 "958a2c2b2392b905cf39d75a3007adb47a818815a8485ca2170b4214233f6b14"
+  url "https://github.com/ryanmjacobs/c/archive/v0.12.tar.gz"
+  sha256 "invalid"
 
   def install
     bin.install "c"
   end
 
   test do
+    #system "#{bin}/c", "")
     pipe_output("#{bin}/c", "int main(void){return 0;}", 0)
   end
 end
