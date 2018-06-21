a63a5d9b460f49ca11a7c46a3e2cada72da95c2d
cielavenir <cielartisan@gmail.com>
2018-06-22 08:26:19 +0900

    Updated Homebrew Formula

--- c.rb
+++ c.rb
@@ -1,7 +1,8 @@
 class C < Formula
   homepage "https://github.com/ryanmjacobs/c"
   url "https://github.com/ryanmjacobs/c/archive/v0.11.tar.gz"
-  sha256 "invalid"
+  sha256 "19b932e0087acf6c639cc5a4fa9a0c87314e62b0561f5517fd7a78e32fb61801"
+  head "https://github.com/ryanmjacobs/c.git"
 
   def install
     bin.install "c"
