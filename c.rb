a80a132a70f53b243913c945496ac5f6b6aa441a
Ryan Jacobs <ryan@rmj.us>
2020-05-05 09:50:43 -0700

    homebrew hash v0.14

--- c.rb
+++ c.rb
@@ -1,7 +1,7 @@
 class C < Formula
   homepage "https://github.com/ryanmjacobs/c"
   url "https://github.com/ryanmjacobs/c/archive/v0.14.tar.gz"
-  sha256 "TODO"
+  sha256 "2b66d79d0d5c60b8e6760dac734b8ec9a7d6a5e57f033b97086821b1985a870b"
   head "https://github.com/ryanmjacobs/c.git"
 
   def install
