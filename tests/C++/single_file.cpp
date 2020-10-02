f2df8393a3da4c0b26165f763e71744abf52e56e
Mayank SIngh <30663174+pk0011@users.noreply.github.com>
2020-10-02 11:25:59 +0530

    This is more understandable for begineers.

--- tests/C++/single_file.cpp
+++ tests/C++/single_file.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i <= 3; i++)
         puts(argv[i]);
 
     printf("%.0f\n", pow(2, 10));
