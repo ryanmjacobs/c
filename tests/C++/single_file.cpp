e59105fdbe17a3d0e42876f7b8c39dc8f234c732
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-06-28 18:05:00 +0400

    Update single_file.cpp
    Key Points:
    •	The usage message is clearer.
    •	size_t is standard for array indices.
    •	EXIT_SUCCESS and EXIT_FAILURE improve code portability.
    •	Minor error check for pow, but in this case it's safe.
    •	The code is easier to maintain and read.

--- tests/C++/single_file.cpp
+++ tests/C++/single_file.cpp
@@ -1,21 +1,25 @@
 #!../../c -Wall -Werror -lm --
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    unsigned int i;
-
-    printf("argc=%d\n", argc);
-
+int main(int argc, const char *argv[]) {
     if (argc != 4) {
-        fputs("error: need 3 arguments\n", stderr);
-        return 1;
+        fprintf(stderr, "Usage: %s <arg1> <arg2> <arg3>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    for (i = 0; i < 4; i++)
+    printf("argc = %d\n", argc);
+    for (size_t i = 0; i < (size_t)argc; i++) {
         puts(argv[i]);
+    }
 
-    printf("%.0f\n", pow(2, 10));
+    double result = pow(2, 10);
+    if (result == 0 && 2 != 0) { // Simple check for pow failure, optional
+        fprintf(stderr, "Error computing power.\n");
+        return EXIT_FAILURE;
+    }
+    printf("%.0f\n", result);
 
-    return 123;
+    return EXIT_SUCCESS;
 }
